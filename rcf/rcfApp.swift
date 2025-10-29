e9301fcd6a14b59560916c8c87463553da82331b
bohzio <mattia.corradi.tr@gmail.com>
2025-10-29 22:02:03 +0100

    Initial Commit

---
+++ rcf/rcfApp.swift
@@ -0,0 +1,17 @@
+//
+//  rcfApp.swift
+//  rcf
+//
+//  Created by Mattia Corradi on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct rcfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
